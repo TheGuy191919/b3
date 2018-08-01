e925e2cbe7a01a508dfbb520df0ec3315b759325
YiwenDong <dong.yiw@husky.neu.edu>
2018-07-31 20:32:12 -0400

    Initial setup of react and spring

---
+++ src/main/java/io/github/cs3200/izaakyiwen/b3/ReactController.java
@@ -0,0 +1,13 @@
+package io.github.cs3200.izaakyiwen.b3;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ReactController {
+
+    @RequestMapping("/*")
+    public String index() {
+        return "/html/index.html";
+    }
+}
