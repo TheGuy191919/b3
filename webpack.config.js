286fa23337a9a385820bd17b57553ca08194f7bb
YiwenDong <dong.yiw@husky.neu.edu>
2018-07-31 21:54:16 -0400

    Fixed styling

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
-var path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
 
 var node_dir = __dirname + '/node_modules';
-const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: './src/main/js/index.js',
@@ -12,48 +12,46 @@ module.exports = {
         filename: './src/main/resources/static/built/bundle.js'
     },
     module: {
-        rules: [
-          {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-              loader: "babel-loader",
+        rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
                     options: {
-                      presets: ["env", "react"]
+                        presets: ["env", "react"]
                     }
-            }
-          },
-          {
-            test: /\.css$/,
-            use: [
-              {
-                loader: "style-loader"
-              },
-              {
-                loader: "css-loader",
-                options: {
-                  modules: true,
-                  importLoaders: 1,
-                  localIdentName: "[name]_[local]_[hash:base64]",
-                  sourceMap: true,
-                  minimize: true
                 }
-              }
-            ]
-          },
-          {
-            test: /\.svg$/,
-            exclude: /(node_modules)/,
-            use: {
-                loader: 'svg-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [{
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader",
+                        options: {
+                            modules: true,
+                            importLoaders: 1,
+                            localIdentName: "[name]_[local]_[hash:base64]",
+                            sourceMap: true,
+                            minimize: true
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.svg$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'svg-loader'
+                }
             }
-          }
         ]
     },
     plugins: [
         new HtmlWebPackPlugin({
-        template: "./src/main/resources/static/html/index.html",
-        filename: "./index.html"
-      })
+            template: "./src/main/resources/static/html/index.html",
+            filename: "./index.html"
+        })
     ]
 };
